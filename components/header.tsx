bc296ded1e64d1cb8f4acbc40acba8e45c17693a
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-29 09:33:12 +0200

    OP-8 add animation to links in floating nav bar

--- components/header.tsx
+++ components/header.tsx
@@ -22,9 +22,11 @@ export default function Header() {
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium
            text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
-            <li className="h-3/4 flex items-center justify-center" key={link.hash}>
+            <motion.li className="h-3/4 flex items-center justify-center" key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}>
               <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition" href={link.hash}>{link.name}</Link>
-            </li>
+            </motion.li>
           ))}
         </ul>
       </nav>
