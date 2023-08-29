02d84ddebd976b3251ef3c31887736c40cab020a
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-29 12:49:45 +0200

    OP-10 add first intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+import Intro from "@/components/intro";
 
-  </main>
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+    </main>
+  )
 }
