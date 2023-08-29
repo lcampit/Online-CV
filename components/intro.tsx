06b0cf0d796f23a017afed1558b30c9f3d705e77
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-29 15:18:19 +0200

    OP-10 add about me section

--- components/intro.tsx
+++ components/intro.tsx
@@ -42,7 +42,7 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <motion.p className="mb-10 mt-4 px-4 text-2x1 font-medium !leading-[1.5] sm:text-4xl"
+      <motion.h1 className="mb-10 mt-4 px-4 text-2x1 font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}>
         {/* TODO update this with actual information */}
@@ -51,7 +51,7 @@ export default function Intro() {
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
       <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
