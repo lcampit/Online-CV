c4359ec00bcc11f8a686b76d883bb343e013aeec
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-09-04 17:52:46 +0200

    add inner card animations

--- components/projects.tsx
+++ components/projects.tsx
@@ -25,7 +25,8 @@ type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8">
+    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]
+      mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
       <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
@@ -35,7 +36,9 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
           ))}
         </ul>
       </div>
-      <Image className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40" src={imageUrl} alt="Project I worked on" quality={95} />
+      <Image className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40
+        group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:rotate-2 transition group-hover:scale-[1.04]
+        group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2" src={imageUrl} alt="Project I worked on" quality={95} />
     </section>
   )
 }
