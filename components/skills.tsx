7e0242c471bf211cf866aee0330c6f7804d94b4d
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-10-06 19:47:45 +0200

    add id to skill component

--- components/skills.tsx
+++ components/skills.tsx
@@ -26,6 +26,7 @@ export default function Skills() {
   return (
     <section
       ref={ref}
+      id="skills"
       className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'>
       <SectionHeading>My Skills</SectionHeading>
       <ul className='flex flex-wrap justify-center gap-2 text-lg text-gray-800'>
