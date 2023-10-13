4506cd338d6b35123017b93059384f0461111952
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-10-13 15:45:03 +0200

    add borderBlack css property

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import { useSectionInView } from '@/lib/hooks';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+import Image from "next/image";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
-import { FaGithubSquare } from 'react-icons/fa';
-import { HiDownload } from 'react-icons/hi';
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
   return (
-    <section ref={ref} className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]' id="home">
+    <section
+      ref={ref}
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      id="home"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           {/* TODO update with proper image and alt text*/}
@@ -21,31 +27,38 @@ export default function Intro() {
             transition={{
               type: "tween",
               duration: 0.2,
-            }}>
-            <Image src='https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100'
+            }}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
               alt="Placeholder portrait"
               width="192"
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl" />
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
           </motion.div>
-          <motion.span className="text-4xl absolute bottom-0 right-0"
+          <motion.span
+            className="text-4xl absolute bottom-0 right-0"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
-              type: 'spring',
+              type: "spring",
               stiffness: 125,
               delay: 0.1,
               duration: 0.7,
-            }}>
+            }}
+          >
             👋
           </motion.span>
         </div>
       </div>
-      <motion.h1 className="mb-10 mt-4 px-4 text-2x1 font-medium !leading-[1.5] sm:text-4xl"
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2x1 font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}>
+        animate={{ opacity: 1, y: 0 }}
+      >
         {/* TODO update this with actual information */}
         <span className="font-bold">Hello, I&apos;m Ricardo.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
@@ -53,33 +66,54 @@ export default function Intro() {
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
-      <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
           delay: 0.1,
-        }}>
-        <Link href="#contact"
+        }}
+      >
+        <Link
+          href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex 
           items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950
-          active:scale-105 transition">
-          Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
-          focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
-          href="/CV.pdf" download> Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
+          focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
+          href="/CV.pdf"
+          download
+        >
+          {" "}
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
-        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15]
-          active:scale-105 transition cursor-pointer border border-black/10 hover:text-gray-950"
-          href="https://linkedin.com" target="_blank">
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15]
+          active:scale-105 transition cursor-pointer borderBlack hover:text-gray-950"
+          href="https://linkedin.com"
+          target="_blank"
+        >
           <BsLinkedin />
         </a>
-        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] outline-none hover:scale-[1.15]
-          active:scale-105 transition cursor-pointer border border-black/10 hover:text-gray-950"
-          href="https://github.com" target="_blank">
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] outline-none hover:scale-[1.15]
+          active:scale-105 transition cursor-pointer borderBlack hover:text-gray-950"
+          href="https://github.com"
+          target="_blank"
+        >
           <FaGithubSquare />
         </a>
       </motion.div>
-    </section >
-  )
+    </section>
+  );
 }
