07351282c5a06ee9c4645fddad02ae3898e678e2
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-11-09 17:00:57 +0100

    add toast for messages

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
 import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -33,11 +34,11 @@ export default function Contact() {
         action={async (formData: FormData) => {
           const { data, error } = await sendEmail(formData);
           if (error) {
-            alert(error);
+            toast.error(error);
             return;
           }
 
-          alert("Email sent successfully!");
+          toast.success("Email sent successfully!");
         }}
       >
         <input
