9ed6dfd702ee07ba0d6a3397974552c7935e3a84
Leo-Campo <leonardo932.campitelli@gmail.com>
2024-01-09 14:30:05 +0100

    update personal information

--- components/skills.tsx
+++ components/skills.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { skillsData } from "@/lib/data";
+import { skillsData, additionalSkillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
 
@@ -47,6 +47,26 @@ export default function Skills() {
           </motion.li>
         ))}
       </ul>
+      <h5 className="text-2xl font-medium capitalize mb-8 text-center mt-5">
+        Additional skills
+        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800 mt-5">
+          {additionalSkillsData.map((skill, index) => (
+            <motion.li
+              className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+              key={index}
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{
+                once: true,
+              }}
+              custom={index}
+            >
+              {skill}
+            </motion.li>
+          ))}
+        </ul>
+      </h5>
     </section>
   );
 }
