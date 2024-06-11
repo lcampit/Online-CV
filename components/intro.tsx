3d6be426dca2cd3b98cf400424d8ab8ed952f7ee
Leonardo Campitelli <leonardo932.campitelli@gmail.com>
2024-06-11 19:08:56 +0200

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -101,7 +101,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="www.linkedin.com/in/leonardo-campitelli"
+          href="https://linkedin.com/in/leonardo-campitelli"
           target="_blank"
         >
           <BsLinkedin />
