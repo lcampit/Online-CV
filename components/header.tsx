a762ab2b11db7078ed778dbdb74323848ceec2b3
Leo-Campo <leonardo932.campitelli@gmail.com>
2025-05-10 20:04:52 +0200

    fix: add header height for different screen sizes

--- components/header.tsx
+++ components/header.tsx
@@ -15,11 +15,16 @@ export default function Header() {
     <header className="z-[999] relative">
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 
-        bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] 
-        sm:w-[48rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] 
+        xl:top-6 xl:h-[3.25rem] xl:w-[48rem] xl:rounded-full 
+        lg:top-6 lg:h-[4.5rem] lg:w-[48rem] lg:rounded-full 
+        md:top-6 md:h-[4.5rem] md:w-[48rem] md:rounded-full 
+        sm:top-6 sm:h-[3.25rem] sm:w-[48rem] sm:rounded-full 
+        dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
-      ></motion.div>
+      >
+      </motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
@@ -55,7 +60,8 @@ export default function Header() {
                       stiffness: 380,
                       damping: 30,
                     }}
-                  ></motion.span>
+                  >
+                  </motion.span>
                 )}
               </Link>
             </motion.li>
